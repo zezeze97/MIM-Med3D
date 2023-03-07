bca60421f9663aa05f6ff89d3bc9910d9a6dd403
zhangzeren <zhangzeren@baidu.com>
2023-03-07 18:07:44 +0800

    support modelnet 40

--- code/data/__init__.py
+++ code/data/__init__.py
@@ -1,6 +1,7 @@
 from .btcv_dataset import BTCVDataset
 from .brats_dataset import BratsDataset
 from .ScanObjectNN_dataset import ScanObjDataset
+from .modelNet_dataset import ModelNetDataset
 from .utils import (
     list_splitter,
     get_modalities,
