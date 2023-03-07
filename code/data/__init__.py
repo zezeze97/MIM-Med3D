2a3a4d67055afaaabba9ffa954d8c46be19ca4ef
zhangzeren <zhangzeren@baidu.com>
2023-03-07 14:08:11 +0800

    debug

--- code/data/__init__.py
+++ code/data/__init__.py
@@ -1,5 +1,6 @@
 from .btcv_dataset import BTCVDataset
 from .brats_dataset import BratsDataset
+from .ScanObjectNN_dataset import ScanObjDataset
 from .utils import (
     list_splitter,
     get_modalities,
